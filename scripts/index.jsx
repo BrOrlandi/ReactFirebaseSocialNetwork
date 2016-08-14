a623972bb2bf1673c6801f51491d0714784f8902
BrOrlandi <brorlandi@gmail.com>
2016-08-14 17:42:27 -0300

    Firebase, Materialize and React dependencies.

--- scripts/index.jsx
+++ scripts/index.jsx
@@ -1,5 +1,11 @@
+var $ = window.jQuery = require('jquery');
+require('materialize-css/bin/materialize.js');
+
 import ReactDOM from 'react-dom';
 import React from 'react';
+
+// Import firebase
+
 
 class App extends React.Component {
     render(){
