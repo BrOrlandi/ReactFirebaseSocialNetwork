cbe76007208bd045ed69614036001f3c97470cb3
BrOrlandi <brorlandi@gmail.com>
2016-08-14 22:09:34 -0300

    More on Navbar, buttons and profile.

--- scripts/components/Navbar.jsx
+++ scripts/components/Navbar.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 import logo from '../../assets/images/logo-semcomp-branco.svg';
 
+import defaultPic from '../../assets/images/profile-placeholder.png';
+
 export default class Navbar extends React.Component {
 
     signIn(){
         console.log("SIGN IN");
+    }
+
+    signOut(){
+        console.log("SIGN OUT");
+    }
+
+    addFriend(){
+        console.log("ADD FRIEND");
     }
 
     render(){
@@ -18,8 +28,11 @@ export default class Navbar extends React.Component {
                                 Semcomp Social
                         </a>
                         <ul className="right hide-on-med-and-down">
-                          <li><a href="#">Bruno Orlandi</a></li>
-                          <li><a href="#" onClick={this.signIn}><i className="material-icons left">account_circle</i>SIGN IN</a></li>
+                            <li><a href="#" onClick={this.addFriend}><i className="material-icons left">person_add</i>Add Friends</a></li>
+                            <li><img className="responsive-img circle navbarPic" src={defaultPic} /></li>
+                            <li><a href="#">Bruno Orlandi</a></li>
+                            <li><a href="#" onClick={this.signIn}><i className="material-icons left">account_circle</i>SIGN IN</a></li>
+                            <li><a className="navButton" href="#" onClick={this.signOut}>SIGN OUT</a></li>
                         </ul>
                     </div>
                 </div>
