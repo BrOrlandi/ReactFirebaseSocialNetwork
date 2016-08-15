f660befccf60612118ff732efa76c746dcdeebb4
BrOrlandi <brorlandi@gmail.com>
2016-08-15 03:37:42 -0300

    Chat Message Board

--- scripts/components/Chat.jsx
+++ scripts/components/Chat.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
 
+import FriendList from './FriendList';
+import MessagesBoard from './MessagesBoard';
+
 export default class Chat extends React.Component {
 
+    // constructor(props) {
+    //     super(props)
+    //     this.state ={
+    //         friend: null
+    //     }
+    // }
+
+
+    // onSelectConversation = (friend)=>{
+    //     this.setState({ friend});
+    // }
+
     render(){
-        return (
-            <div className="container"></div>
-        )
+        if(!this.props.userCheck){
+            return <div className="container"></div>;
+        }
+        if(this.props.currentUser){
+            return(
+                <div className="chatContainer">
+                    {/* Friend List... */}
+                    {/* <FriendList /> */}
+                    {/* onSelectConversation={this.onSelectConversation} friend={this.state.friend} */}
+
+                    {/* All message stuff is handled here */}
+                    <MessagesBoard  />
+                    {/* friend={this.state.friend} */}
+                </div>
+            );
+        }else{
+            return(
+            <div className="container">
+                <div className="section">
+                    <div className="row">
+                        <div className="col s12 m4 offset-m4">
+                            <div className="card-panel white center-align">
+                                <p className="flow-text">Sign in to the Social Network.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            )
+        }
     }
 }
---
+++ scripts/components/MessagesBoard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+// import MessageList from './MessageList';
+// import MessageForm from './MessageForm';
+
+class MessageBoard extends React.Component {
+    render(){
+        if(!this.props.friend){
+            return (<div className="messages">
+                    <div className="card-panel white center-align">
+                        Select an conversation on the left.
+                    </div>
+                </div>);
+        }
+
+        return(
+            <div className="messages">
+                {/* Here goes the messages */}
+                {/* <MessageList {...this.props}/> */}
+
+                {/* Here goes the form to send messages */}
+                {/* <MessageForm {...this.props} /> */}
+            </div>
+        );
+    }
+}
+
+
+export default MessageBoard;
